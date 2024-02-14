be51aecce14587559729e4e85963ae1533006c62
5k01 <figuerooso1996@gmail.com>
2024-02-14 16:10:49 -0600

    Added cards from bootstrap

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ db.connect();
 app.get("/", async (req, res) => {
     let books = []
     try {
-        const result = await db.query("SELECT * FROM books b JOIN authors a ON  b.author_id=a.id ORDER BY id DESC");
+        const result = await db.query("SELECT b.isbn, b.title, b.publication_year, b.pages, b.resume, b.score, a.name AS author_name, COUNT(c.id) AS comments FROM books b JOIN authors a ON b.author_id = a.id LEFT JOIN comments c ON b.isbn = c.book_id GROUP BY b.isbn, b.title, b.publication_year, b.pages, b.resume, b.score, a.name ORDER BY b.publication_year ASC; ");
         books = result.rows;
     } catch (error) {
         console.log(`Error al obtener los libros ${error}`);
@@ -40,3 +40,14 @@ app.get("/", async (req, res) => {
 app.listen(port, () => {
     console.log(`App is runngin on http://localhost:${port}/`);
 });
+
+function getStarRating(score) {
+    const maxStars = 10;
+    const filledStars = Math.round((score / 10) * maxStars);
+    const emptyStars = maxStars - filledStars;
+    const starHTML = '<span class="star">&#9733;</span>'; // Unicode for star symbol
+    const emptyStarHTML = '<span class="star">&#9734;</span>'; // Unicode for empty star symbol
+    return starHTML.repeat(filledStars) + emptyStarHTML.repeat(emptyStars);
+}
+
+app.locals.getStarRating = getStarRating;
