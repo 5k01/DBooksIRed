6709efaf7becdfcb9a729a330ce59c728a8d1e2b
5k01 <figuerooso1996@gmail.com>
2024-02-13 21:34:49 -0600

    Create tables.sql

---
+++ sql/tables.sql
@@ -0,0 +1,31 @@
+CREATE TABLE authors (
+    id SERIAL PRIMARY KEY, name VARCHAR(100)
+);
+
+CREATE TABLE books (
+    isbn BIGINT UNIQUE NOT NULL, -- ISBN-13 number.
+    title VARCHAR(255), -- Book's title.
+    publication_year INTEGER CHECK (publication_year >= 0), -- Publication year.
+    author_id INTEGER REFERENCES authors (id) -- Author of the book.
+    ON DELETE CASCADE, -- If an author gets deleted, remove all their books too.
+    pages INT NOT NULL,
+    resume TEXT,
+    score INT
+);
+
+CREATE TABLE comments(
+    id SERIAL PRIMARY KEY,
+    text TEXT NOT NULL,
+    date DATE,
+    book_id INTEGER REFERENCES books(isbn) ON DELETE CASCADE
+);
+
+CREATE TABLE genres (
+    id SERIAL PRIMARY KEY, name VARCHAR(100)
+);
+
+CREATE TABLE book_genres(
+    id SERIAL PRIMARY KEY,
+    book_id INTEGER REFERENCES books(isbn) ON DELETE CASCADE,
+    genre_id INTEGER REFERENCES genres(id)
+);
